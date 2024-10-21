fbd2d07607725b29dee28e5b19b5767893170917
Max <maxmilian.janda@gmail.com>
2024-10-21 23:56:39 +0200

    LastCommitOdTheDay

--- main.js
+++ main.js
@@ -1,24 +1,45 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-setupCounter(document.querySelector('#counter'))
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(5);
+camera.position.setY(1);
+
+let tunnel;
+const loader = new GLTFLoader();
+loader.load(
+  "tunnel_test.glb",
+  function (gltf) {
+    tunnel = gltf.scene;
+    tunnel.position.y = 0;
+    tunnel.position.x = -0.5;
+    tunnel.rotation.y = 1.5;
+    scene.add(tunnel);
+  },
+  function (xhr) {},
+  function (error) {}
+);
+
+const topLight = new THREE.DirectionalLight(0xfffff);
+topLight.position.set(500, 500, 500);
+scene.add(topLight);
+
+const reReander = () => {
+  requestAnimationFrame(reReander);
+  renderer.render(scene, camera);
+};
+reReander();
